1c2b2e970dfbbab98e2fa14c7e48dad9a313249e
IrfaSheikh <irfasheikh@gmail.com>
2021-10-12 11:00:29 -0500

    gameflow class start

---
+++ src/main/java/blackjack/gameFlow.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+public class gameFlow implements IGameFlow  {
+        private IDeck deck;
+        private IBlackJackLogic blackJackLogic;
+        private IHand hand;
+
+        public gameFlow (IDeck deck, IBlackJackLogic blackJackLogic, IHand hand){
+            this.deck = deck;
+            this.blackJackLogic = blackJackLogic;
+            this.hand = hand;
+        }
+
+        public Boolean playGame(){
+            if
+            return true;
+        }
+
+
+
+}
