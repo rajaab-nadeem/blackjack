29cf77206803b6dfe68d0c6fdf653975edf0d585
IrfaSheikh <irfasheikh@gmail.com>
2021-10-12 12:47:26 -0500

    more gameflow

--- src/main/java/blackjack/gameFlow.java
+++ src/main/java/blackjack/gameFlow.java
@@ -3,15 +3,18 @@ package blackjack;
 public class gameFlow implements IGameFlow  {
         private IDeck deck;
         private IBlackJackLogic blackJackLogic;
-        private IHand hand;
+        private IHand handDealer;
+        private IHand handPlayer;
 
-        public gameFlow (IDeck deck, IBlackJackLogic blackJackLogic, IHand hand){
+        public gameFlow (IDeck deck, IBlackJackLogic blackJackLogic, IHand handDealer, IHand handPlayer){
             this.deck = deck;
             this.blackJackLogic = blackJackLogic;
-            this.hand = hand;
+            this.handDealer = handDealer;
+            this.handPlayer = handPlayer;
         }
 
         public Boolean playGame(){
+            deck.shuffle();
             blackJackLogic.startingMove();
 
             return true;
