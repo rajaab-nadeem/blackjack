8cba7a0413923ffce0e4d0280dac5be1b0b53d0f
roberticold <70712597+roberticold@users.noreply.github.com>
2021-10-13 09:21:30 -0500

    CheckForWinner

--- src/main/java/blackjack/checkForWinner.java
+++ src/main/java/blackjack/checkForWinner.java
@@ -4,8 +4,16 @@ public class checkForWinner implements ICheckForWinner{
 
 
     public void checkIfWinner(ITotalHandValues THValue, IHand dealer, IHand player){
+
+
+        if(THValue.totalhandvalues(dealer)==21 && THValue.totalhandvalues(player)==21 ){
+            System.out.println("It is a TIE");
+
+
+        }
+
         if(THValue.totalhandvalues(player)==21){
-            System.out.println(player.getName() + " WON!!!");
+            System.out.println(player.getName() + " Wins!!!");
 
 
         }
@@ -14,6 +22,39 @@ public class checkForWinner implements ICheckForWinner{
 
 
         }
+
+        if(THValue.totalhandvalues(dealer)>21){
+            System.out.println("Dealer" + "Busted!!!");
+
+
+        }
+
+        if(THValue.totalhandvalues(dealer)==21) {
+            System.out.println("Dealer" + " Wins!!!");
+
+        }
+
+
+        if(THValue.totalhandvalues(dealer)==THValue.totalhandvalues(player) ){
+            System.out.println("It is a TIE");
+
+
+        }
+
+        if(THValue.totalhandvalues(dealer)>THValue.totalhandvalues(player) ){
+            System.out.println("Dealer Wins!!");
+
+
+        }
+
+        if(THValue.totalhandvalues(dealer)<THValue.totalhandvalues(player) ){
+            System.out.println(player.getName() + "Wins!!");
+
+
+        }
+
+
+
 
 
     }
